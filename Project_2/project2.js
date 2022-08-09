9a2835efd9068da527b716919071f74027438d2f
quinfiff <109001822+quinfiff@users.noreply.github.com>
2022-08-09 15:19:44 -0400

    Add files via upload

--- Project_2/project2.js
+++ Project_2/project2.js
@@ -1,11 +1,26 @@
+//modal modalWindow
+const item1 = document.querySelector('#item1')
+const closeModal = document.querySelector('#closeModal')
+const modalWindow = document.querySelector('.modalWindow')
+
+item1.addEventListener('click', function(){
+  modalWindow.style.display = 'block';
+})
+closeModal.addEventListener('click', function(){
+  modalWindow.style.display = 'none';
+})
+
+
+
 //automatic slideshow
 const slideAuto = document.querySelectorAll('.slideAuto')
   let indexSlide = 0;
 slideshowAuto()
 function slideshowAuto(){
   let numSlides = slideAuto.length;
-  if(indexSlide>numSlides){indexSlide=0}
-  if(indexSlide<0){indexSlide=numSlides - 1;}
+console.log(`counter=${indexSlide}`)
+  if(indexSlide>=numSlides){indexSlide=0; console.log(`indexSlide=${indexSlide}`)}
+  if(indexSlide<0){indexSlide=numSlides - 1;console.log(`indexSlide<1=${indexSlide}`)}
   for(let eachIndex = 0; eachIndex<numSlides; eachIndex++){
     slideAuto[eachIndex].style.display = 'none'
   }
