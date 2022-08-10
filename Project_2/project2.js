04d6ee9b5a177de4ec2d45778bddf5e5372d0aac
quinfiff <109001822+quinfiff@users.noreply.github.com>
2022-08-09 23:17:42 -0400

    Add files via upload

--- Project_2/project2.js
+++ Project_2/project2.js
@@ -25,18 +25,67 @@ closeModal.addEventListener('click', function(){
 })
 
 //modal 2
-/*const item2 = document.querySelector('#item2')
+const item2 = document.querySelector('#item2')
 const closeModal2 = document.querySelector('#closeModal2')
-const modalWindow = document.querySelector('.modalWindow')
+const modalWindow2 = document.querySelector('.modalWindow2')
 
 item2.addEventListener('click', function(){
-  modalWindow.style.display = 'block';
+  modalWindow2.style.display = 'block';
 })
 closeModal2.addEventListener('click', function(){
-  modalWindow.style.display = 'none';
-})*/
+  modalWindow2.style.display = 'none';
+})
 
-//automatic slideshow
+//modal 3
+const item3 = document.querySelector('#item3')
+const closeModal3 = document.querySelector('#closeModal3')
+const modalWindow3 = document.querySelector('.modalWindow3')
+
+item3.addEventListener('click', function(){
+  modalWindow3.style.display = 'block';
+})
+closeModal3.addEventListener('click', function(){
+  modalWindow3.style.display = 'none';
+})
+
+//modal 4
+const item4 = document.querySelector('#item4')
+const closeModal4 = document.querySelector('#closeModal4')
+const modalWindow4 = document.querySelector('.modalWindow4')
+
+item4.addEventListener('click', function(){
+  modalWindow4.style.display = 'block';
+})
+closeModal4.addEventListener('click', function(){
+  modalWindow4.style.display = 'none';
+})
+
+//modal 5
+const item5 = document.querySelector('#item5')
+const closeModal5 = document.querySelector('#closeModal5')
+const modalWindow5 = document.querySelector('.modalWindow5')
+
+item5.addEventListener('click', function(){
+  modalWindow5.style.display = 'block';
+})
+closeModal5.addEventListener('click', function(){
+  modalWindow5.style.display = 'none';
+})
+
+//modal 6
+const item6 = document.querySelector('#item6')
+const closeModal6 = document.querySelector('#closeModal6')
+const modalWindow6 = document.querySelector('.modalWindow6')
+
+item6.addEventListener('click', function(){
+  modalWindow6.style.display = 'block';
+})
+closeModal6.addEventListener('click', function(){
+  modalWindow6.style.display = 'none';
+})
+
+
+//slideshow
 const slideAuto = document.querySelectorAll('.slideAuto')
   let indexSlide = 0;
 slideshowAuto()
